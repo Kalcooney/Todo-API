2878073ca7282053817687f1dcbf5b202fc6b856
Kaleb Cooper <kalebcooper@gmail.com>
2017-07-29 18:45:03 +1200

    Add POST Todos route

---
+++ server/models/todo.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+let todoSchema = mongoose.Schema({
+    text: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    completedAt: {
+        type: Number,
+        default: null
+    }
+});
+
+let Todo = mongoose.model("Todo", todoSchema);
+
+module.exports = {Todo};
